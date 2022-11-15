6385a40e3cafc060918a0651a9e4841d6a7067ad
seatwiggy <93173836+seatwiggy@users.noreply.github.com>
2022-11-15 08:40:45 -0700

    Initial commit

---
+++ src/main/java/com/bird/flightless/movienighterfrontend/MovieNighterFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.bird.flightless.movienighterfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieNighterFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieNighterFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bird/flightless/movienighterfrontend/MovieNighterFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bird.flightless.movienighterfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieNighterFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
