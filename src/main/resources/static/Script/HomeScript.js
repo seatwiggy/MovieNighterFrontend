79fc159d795bfe69a858f39ee046c5f7697914bf
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2022-12-01 09:01:36 -0700

    HomeScript.js

--- src/main/resources/static/Script/HomeScript.js
+++ src/main/resources/static/Script/HomeScript.js
@@ -1,32 +1,19 @@
 function voteForMovie(){
-    let movieName = document.getElementById("movieName").value;
+    let movieId = getMovieIdFromAPI(document.getElementById("movieName").value);
 
-    let movie = {
-        "id":0,
-        "name":name,
-        "firstVideoAppearance":fva
-    }
-    //ajax
     let xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("POST","http://localhost:8081/character/");
+
+    xmlHttp.open("POST","http://localhost:8081/groups/vote/"+"/"+groupID+"/"+movieId);
     xmlHttp.setRequestHeader("Content-Type","application/json");
     xmlHttp.setRequestHeader("Authorization",localStorage.getItem("auth"));
     xmlHttp.onreadystatechange = function (){
         console.log("ready state: ", this.status);
         if(this.readyState == XMLHttpRequest.DONE && this.status ==200){
-            //the character was created
-            console.log("succesfully posted character");
-            getCharacters();
-        } else if (this.readyState == XMLHttpRequest.DONE && this.status == 401){
-            console.log("create failed");
-            document.getElementById("characterErrorMessage").innerHTML = "You are not authroized to create a Character, please login";
-
-        } else if(this.readyState == XMLHttpRequest.DONE && this.status == 403){
-            document.getElementById("characterErrorMessage").innerHTML = "You do not have the correct permissions to create Characters";
+            getMoviesFromDatabase();
         }
         xmlHttp.send();
     }
-    xmlHttp.send(JSON.stringify(vCharacter));
+    xmlHttp.send();
 }
 
 function addMovieToList(){
@@ -38,6 +25,10 @@ function displayMovies(){
 }
 
 function getMoviesFromDatabase(){
+
+}
+
+function getMovieIdFromAPI(){
 
 }
 
