7e7f9cb8e6ae33a843193edd7ee921ce6ee68004
EmileRandolph <98546063+EmileRandolph@users.noreply.github.com>
2022-12-02 10:43:35 -0700

    HomeScript.js

--- src/main/resources/static/Script/HomeScript.js
+++ src/main/resources/static/Script/HomeScript.js
@@ -1,17 +1,17 @@
 const apiKey = "";
+let groupID = localStorage.getItem("groupID");
 
 function voteForMovie() {
 	let movieId = getMovieIdFromAPI(document.getElementById("movieName").value);
 
 	let xmlHttp = new XMLHttpRequest();
 
-	xmlHttp.open("POST", "http://localhost:8081/groups/vote/" + "/" + groupID + "/" + movieId);
-	xmlHttp.setRequestHeader("Content-Type", "application/json");
+	xmlHttp.open("POST", "http://localhost:8081/groups/vote/" + groupID + "/" + movieId);
 	xmlHttp.setRequestHeader("Authorization", localStorage.getItem("auth"));
-	xmlHttp.onreadystatechange = function () {
+	xmlHttp.onreadystatechange = async function () {
 		console.log("ready state: ", this.status);
 		if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-			getMoviesFromDatabase();
+			await displayMovies();
 		}
 		xmlHttp.send();
 	}
@@ -20,31 +20,53 @@ function voteForMovie() {
 
 function addMovieToList() {
 
+    console.log(localStorage.getItem("auth"));
+    let movie = getMovieIdFromAPI(document.getElementById(movieName).value);
+    //ajax
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST","http://localhost:8081/groups/addMovie" + groupID + "/" + movie);
+    xmlHttp.setRequestHeader("Authorization",localStorage.getItem("auth"));
+    xmlHttp.onreadystatechange = async function (){
+        console.log("ready state: ", this.status);
+        if(this.readyState == XMLHttpRequest.DONE && this.status ==200){
+			await displayMovies();
+        }
+        xmlHttp.send();
+    }
+    xmlHttp.send();
 }
 
-function displayMovies(movies) {
+async function displayMovies() {
+	let movies=await getMoviesFromAPI(getMoviesFromDatabase());
 	let movieList = document.getElementById("movieList");
 	movieList.innerHTML = "";
 	for (let movie of movies) {
-		let movie_html = "<p> Name: " + movie.title + "<br/> Movie Length: " + movie.movieLength + "</p>";
+		let movie_html = "<p> Name: " + movie.title + "<br/> Movie Length: " + movie.runtime + "</p>";
 		movieList.innerHTML += movie_html;
 	}
 }
 
 function getMoviesFromDatabase() {
-	function getMoviesFromDatabase() {
 		let xmlHttp = new XMLHttpRequest();
 		xmlHttp.onreadystatechange = function () {
 			if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
 				let movies = JSON.parse(this.responseText);
-				displayMovies(getMoviesFromAPI(movies));
+				return movies;
 			}
 		}
-		xmlHttp.open("GET", "http://localhost:8081/character/", true);
+		xmlHttp.open("GET", "http://localhost:8081/movies/"+groupID, true);
 		xmlHttp.send();
 	}
 
-	async function getMovieIdFromAPI(title) {
+async function getMoviesFromAPI(movies){
+	let movie_list = [];
+		for (let movie of movies) {
+			movie_list.push(await getMovieDetailsFromAPI(movie.Id));
+		}
+		return movie_list;
+	}
+
+async function getMovieIdFromAPI(title) {
 		fetch(`https://www.omdbapi.com/?apikey=${apiKey}&t=${title}`)
 			.then(response => response.json())
 			.then(data => {
@@ -52,7 +74,7 @@ function getMoviesFromDatabase() {
 			})
 	}
 
-	async function getMovieDetailsFromAPI(movieId) {
+async function getMovieDetailsFromAPI(movieId) {
 		fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`)
 			.then(response => response.json())
 			.then(data => {
@@ -60,11 +82,25 @@ function getMoviesFromDatabase() {
 			})
 	}
 
-	async function searchMovieAPI(search) {
+async function searchMovieAPI(search) {
 		fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`)
 			.then(response => response.json())
 			.then(data => {
 				return data;
 			})
 	}
+
+async function displaySearchedMovies(){
+	let movies= await searchMovieAPI(document.getElementById("movieName"));
+	let movieList = document.getElementById("search_list");
+	movieList.innerHTML = "<p>";
+	for (let movie of movies) {
+		let movie_html = "Name: " + movie.title + "<br/> Movie Length: " + movie.runtime + "<br/> Plot: " +movie.plot;
+		movieList.innerHTML += movie_html;
+	}
+	movieList.innerHTML += <p/>
 }
+
+
+
+
